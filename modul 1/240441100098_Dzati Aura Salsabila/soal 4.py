db1bd1809378a5cfe65340458b6b1fc59d750da7
salsa0406 <salsabiladzatiaura@gmail.com>
2024-10-01 11:58:56 +0700

    240441100098_DzatiAuraSalsabila_MuhammadMaulanaKhanif_Modul1

---
+++ modul 1/240441100098_Dzati Aura Salsabila/soal 4.py
@@ -0,0 +1,26 @@
+#soal 4
+import math
+jumlah_orang = 7
+banyaknya_orang_yang_dipilih = 4
+
+hasil = math.comb(jumlah_orang, banyaknya_orang_yang_dipilih)
+print("cara yang bisa digunakan Darsono sebanyak:", hasil)
+
+#rumus C(n,k)= n!/k!(n−k)!
+
+
+
+
+
+
+
+
+
+#rumus C(n,k)= n!/k!(n−k)!
+#import math berfungsi untuk menggunakan operasi aritmatika
+#math.comb berfungsi untuk melakukan fungsi combinasi
+#fungsi combinasi adalah Rumus untuk menentukan jumlah kelompok berbeda yang mungkin dibentuk dari sejumlah objek yang tersedias
+
+
+
+
