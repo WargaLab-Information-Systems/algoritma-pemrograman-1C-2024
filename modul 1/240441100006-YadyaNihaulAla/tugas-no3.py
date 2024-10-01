a23257a42785fc228a34bdd8c4be52ea1beb15fc
yadyael <yadyanihaulala@gmail.com>
2024-10-01 20:05:18 +0700

    Delete File

--- modul 1/240441100006-YadyaNihaulAla/tugas-no3.py
+++
@@ -1,8 +0,0 @@
-
-nilai_usd = 35
-
-# Kurs dari USD ke IDR tanggal 25 September 2024
-kurs_usd_ke_idr = 15077
-jumlah_idr = nilai_usd * kurs_usd_ke_idr
-
-print("Uang Suraji setelah menukarkan uang USD menjadi Rupiah adalah Rp. ", jumlah_idr)
