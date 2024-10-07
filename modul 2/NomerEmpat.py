eea62944fdafc859de40d666687c961234f6adc9
WildahFirdausiahRofiq <wfirda57@gmail.com>
2024-10-07 21:22:56 +0700

    240441100110-WILDAH FIRDAUSIAH ROFIQ-MUHAMMD MAULANA KHANIF-MODUL2

---
+++ modul 2/NomerEmpat.py
@@ -0,0 +1,7 @@
+tahun = int(input("masukkan tahunnya : "))
+if tahun % 4 == 0 and tahun % 400 == 0:
+    print(f"tahun {tahun} merupakan tahun kabisat")
+elif tahun % 100 == 0:
+    print(f"tahun {tahun} bukan tahun kabisat")
+else :
+    print(f"{tahun}bukan tahun kabisat!")
