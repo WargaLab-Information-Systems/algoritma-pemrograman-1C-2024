d7793857daa4e9f4a5626ee5ba57582a285550a5
SarifatulHasanah <akusarifa2336@gmail.com>
2024-10-11 15:19:04 +0700

    240441100122_SarifatulHasanah_KakIsma_Modul1

--- modul 2/240441100122-SARIFATUL HASANAH/soal4.py
+++ modul 2/240441100122-SARIFATUL HASANAH/soal4.py
@@ -1,7 +1,9 @@
 tahun = int(input("Masukkan tahunnya : "))
-if tahun % 4 == 0 or tahun % 400 == 0 :
+if tahun % 400 == 0 :
     print(f"Tahun {tahun} merupakan tahun kabisat")
 elif tahun % 100 == 0 :
     print(f"Tahun {tahun} bukan tahun kabisat")
+elif tahun % 4 == 0 :
+    print(f"{tahun} merupakan tahun kabisat.") 
 else :
    print(f"{tahun} bukan tahun kabisat!!!")
