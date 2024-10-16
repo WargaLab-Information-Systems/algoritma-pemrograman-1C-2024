ebd1d265839544107ff2ace996b0ed73746542fc
WildahFirdausiahRofiq <wfirda57@gmail.com>
2024-10-16 17:33:50 +0700

    240441100110-Wildah-Firdausiah-Rofiq-modul1-AsprakKakMuhammadMaulanaKhanif

---
+++ UTS/240441100110-WildahFirdausiahRofiq/240441100110_Wildah Firdausiah Rofiq.py
@@ -0,0 +1,33 @@
+makan_mandi = 25 #menit
+jalan_kaki = 30 #mrnit
+motor = 15 #menit
+waktu_tiba_dikampus : 60
+
+ makan = int(input("apakah kamu sudah makan dan sudah mandi?" : ))
+ if belum :
+     print (f"makan dan mandilah dulu dengan waktu {makan_mandi }")
+ else sudah:
+     print ("lanjut berangkat ke kampus")
+
+
+transportasi = int (input(" mau memilih transportasi apa untuk berangkat ke kampus" : ))
+if jalan_kaki:
+   print("jika kamu menggunakan jalan kaki maka kamu akan memakan waktu 15 menit" :)
+else motor :
+   print ("jika kamu menggunakan motor maka kamu akan memakan waktu 10 menit")
+
+transportasi = (makan_mandi+ jalan_kaki)
+print (f"waktu yang digunakan untuk ke kampus adalah ")
+
+
+
+
+
+
+
+
+
+
+
+
+
