15d4555a03e1b9098c4f4ba8d095b794c080b467
Febryan Setya Wibisono <febryansetya5@gmail.com>
2024-10-29 21:23:31 +0700

    240441100134_FebryanSetyaWibisono_Modul4_KAKISMA (#95)
    * coba
    
    * 240441100134_FebryanSetyaWibisono_ASPRAKAKISMA_MODUL1
    
    * 240441100134_FebryanSetyaWibisono_UTS_KAKISMA
    
    * 240441100134_FebryanSetyaWibisono_Modul4_KAKISMA

---
+++ UTS/240441100134_FebryanSetyaWibisono_UTS_KAKISMA/240441100134_FebryanSetyaWIbisono_UTS_KAKISMA.py
@@ -0,0 +1,13 @@
+belum_makan = 15
+belum_mandi = 10
+jalan_kaki = 30
+naik_motor = 15
+
+input(str("sudah makan: "))
+input(str("sudah mandi: "))
+input(str("pilih transportasi: "))
+
+if belum_makan != "ya" :
+    print("15 menit")
+if belum_mandi != "ya" :
+    print("10 menit")
