f6a2d662ee3060cd8926c2ff224b40ae7534ae3e
Srilaminamanik <srilamina25@gmail.com>
2024-10-29 21:12:51 +0700

    240441100138-SriLaminaManik-MODUL2-AsprakKakIsma (#66)
    * 240441100138-SriLaminaManik
    
    * MODUL1_240441100138_SRILAMINAMANIK_Kakisma
    
    * 240441100138-SriLaminaManik-AsprakKakIsma
    
    * 240441100138-SriLaminaManik-MODUL2-AsprakKakIsma
    
    * 240441100138-SRI LAMINA MANIK-ISMA'YAFA NUR ZAMZAMI RAMADHANI-MODUL3
    
    ---------
    
    Co-authored-by: ismaramadhani <143859180+ismaramadhani@users.noreply.github.com>

---
+++ modul 3/240441100138_SriLaminaManik/soal3.py
@@ -0,0 +1,31 @@
+# Menggunakan loop untuk menghitung denda
+while True:
+        
+# Meminta pengguna untuk memasukkan lama penyewaan
+    hari_penyewaan = int(input("Masukkan jumlah hari peminjaman: "))
+    hari_terlambat = int(input("Masukkan jumlah hari keterlambatan: "))
+# Denda Per hari Keterlambatan
+    denda_per_hari = 2500
+            
+# Denda Tambahan jika keterlambatan lebih dari 10 hari
+    denda_tambahan_per_5_hari = 5500
+
+# Menghitung denda dasar
+    denda = hari_terlambat * denda_per_hari
+
+# Jika keterlambatan lebih dari 10 hari, tambahkan denda tambahan
+    if hari_terlambat > 10:
+        
+# Hitung berapa banyak kelipatan 5 hari keterlambatan setelah 10 hari pertama
+        sisa_hari = hari_terlambat - 5
+        denda_tambahan = (sisa_hari // 5) * denda_tambahan_per_5_hari
+        denda += denda_tambahan 
+
+# Menampilkan hasil perhitungan denda
+    print(f"Total denda keterlambatan: Rp {denda}")
+
+# Menanyakan apakah ingin menghitung kembali
+    ulang = input("Apakah Anda ingin menghitung kembali? (y/n): ")
+    if ulang != 'y':
+        print("Terima kasih, sampai jumpa!")
+    break
