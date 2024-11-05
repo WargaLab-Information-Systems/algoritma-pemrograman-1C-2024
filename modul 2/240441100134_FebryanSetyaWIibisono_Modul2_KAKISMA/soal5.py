c2d9a90bf546e372b31b2ab91a74bad39829a285
febryansetya <febryansetya5@gmail.com>
2024-11-05 21:25:30 +0700

    240441100134_FebryanSetyaWibisono_Modul2&3_KAKISMA

---
+++ modul 2/240441100134_FebryanSetyaWIibisono_Modul2_KAKISMA/soal5.py
@@ -0,0 +1,31 @@
+# Input untuk nama pembeli dan usia
+nama_pembeli = input("Masukkan nama pembeli: ")
+usia = int(input("Masukkan usia pembeli: ")) 
+ 
+# Cek usia 
+if usia < 18: 
+    print("Maaf, usia anda belum mencukupi.") 
+else: 
+    # Input total harga 
+    harga = int(input("Masukkan total belanja: Rp ")) 
+    member = input("Apakah anda memiliki kartu member (ya/tidak)? ")
+ 
+    # Cek diskon 
+    diskon = 0 
+    if member == 'ya' and harga >= 500000:
+        diskon += 25 
+    elif member == 'ya' and harga < 500000: 
+        diskon += 15
+    elif member != 'ya' and harga >= 500000: 
+        diskon += 10
+    else:
+        print("Tidak dapat diskon")
+ 
+    # Hitung total harga setelah diskon 
+    total_setelah_diskon = harga - (harga * diskon / 100) 
+
+    # Tampilkan hasil 
+    print("Nama Pembeli: ",nama_pembeli) 
+    print("Diskon yang didapatkan: %",diskon) 
+    print("Total harga sebelum diskon: Rp ",harga) 
+    print("Total harga setelah diskon: Rp ",total_setelah_diskon)
