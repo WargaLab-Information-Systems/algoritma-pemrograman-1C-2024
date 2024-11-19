350d55539223c9870ae9abcf7cd7b26a836ca697
NurDinaKamelia <nurdinakamelia@gmail.com>
2024-11-19 11:46:26 +0700

    240441100154_NurDinaKamelia_Uts_AsprakKakIsma

---
+++ UTS/240441100154_NurDinaKamelia/UTS_240441100154_NurDinaKamelia.py
@@ -0,0 +1,22 @@
+#membuat program "semangat ngampus"
+makan = int(input("masukkan lama makan:"))
+if makan >15:
+    print("anda sudah makan")
+else :
+    print("anda belum makan")
+    
+mandi = int(input("masukkan lama mandi:"))
+if mandi >10:
+    print("anda sudah mandi")
+else :
+    print ("anda belum mandi")
+
+tp = 60
+if tp >60:
+    print("anda memakai motor")
+else :
+    print()
+
+
+
+
