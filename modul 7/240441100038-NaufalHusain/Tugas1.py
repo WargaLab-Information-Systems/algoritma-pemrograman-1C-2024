2e3d2e8cbfbe5503a5f0e52a34d5398f765528f1
Naufalhsain <naufalhusainhusain@gmail.com>
2024-11-25 22:32:05 +0700

    NaufalHusain_240441100038_modul-7_KakAR'RaffiAbqoriNurAzizi

--- modul 7/240441100038-NaufalHusain/Tugas1.py
+++ modul 7/240441100038-NaufalHusain/Tugas1.py
@@ -1,86 +1,9 @@
-# # program peminjaman alat untuk penelitian kesehatan
-# # Membuat dictionary untuk mencatat jumlah alat berdasarkan jenis
-# alat = {}
-
-# # Fungsi tambah alat dengan dua parameter jenis, jumlah dengan type data string
-# def tambah_alat(jenis, jumlah):
-#     if jenis in alat:
-#         alat[jenis] += jumlah
-#     else:
-#         alat[jenis] = jumlah
-
-# # Fungsi kurangi alat dengan dua parameter jenis, jumlah dengan type data string
-# def kurangi_alat(jenis, jumlah):
-#     if jenis in alat:
-#         alat[jenis] -= jumlah
-#         if alat[jenis] <= 0:
-#             del alat[jenis]  # Hapus jika jumlah 0 atau negatif
-
-# # Fungsi untuk tukar alat
-# def tukar_alat(jenis_dikembalikan, jumlah_dikembalikan, jenis_dipinjam, jumlah_dipinjam):
-#     kurangi_alat(jenis_dikembalikan, jumlah_dikembalikan)
-#     tambah_alat(jenis_dipinjam, jumlah_dipinjam)
-
-# # proses inputan dinamis
-# def proses_peminjaman():
-#     while True:
-#         print("\nMenu:")
-#         print("1. Tambahkan alat")
-#         print("2. kembalikan alat")
-#         print("3. Tukar alat")
-#         print("4. Lihat alat yang sedang dipinjam")
-#         print("5. Selesai")
-        
-#         pilihan = input("Pilih opsi (1-5): ")
-
-#         # jika variabel pilihan sama dengan 1 jalankan program di bawah ini
-#         if pilihan == "1":
-#             # program Tambahkan alat
-#             jenis = input("Masukkan jenis alat yang dipinjam: ")
-#             jumlah = int(input(f"Masukkan jumlah {jenis} yang dipinjam: "))
-#             tambah_alat(jenis, jumlah)
-
-#         # jika variabel pilihan tidak dengan 1 apakah variabel pilihan sama dengan 2 jalankan program di bawah ini
-#         elif pilihan == "2":
-#             # program kembalikan alat
-#             jenis = input("Masukkan jenis alat yang dikembalikan: ")
-#             jumlah = int(input(f"Masukkan jumlah {jenis} yang dikembalikan: "))
-
-#             kurangi_alat(jenis, jumlah)
-#         # jika variabel pilihan tidak dengan 3 apakah variabel pilihan sama dengan 4 jalan kan program di bawah ini
-#         elif pilihan == "3":
-#             # program Tukar alat
-#             jenis_dikembalikan = input("Masukkan jenis alat yang dikembalikan: ")
-#             jumlah_dikembalikan = int(input(f"Masukkan jumlah {jenis_dikembalikan} yang dikembalikan: "))
-#             jenis_dipinjam = input("Masukkan jenis alat yang dipinjam: ")
-#             jumlah_dipinjam = int(input(f"Masukkan jumlah {jenis_dipinjam} yang dipinjam: "))
-#             tukar_alat(jenis_dikembalikan, jumlah_dikembalikan, jenis_dipinjam, jumlah_dipinjam)
-        
-#         elif pilihan == "4":
-#             # program melihat alat yang sedang dipinjam
-#             print("\nAlat yang sedang dipinjam:")
-#             if alat:
-#                 for jenis, jumlah in alat.items():
-#                     print(f"- {jenis}: {jumlah} buah alat")
-#             else:
-#                 print("Tidak ada alat yang sedang dipinjam.")
-        
-#         elif pilihan == "5":
-#             # Selesai
-#             print("Proses selesai.")
-#             break
-        
-#         else:
-#             print("Pilihan tidak valid. Coba lagi.")
-
-# # Jalankan program
-# proses_peminjaman()
-
-# Dictionary untuk menyimpan jumlah alat kesehatan
+# program peminjaman alat untuk penelitian kesehatan
+# Membuat dictionary untuk mencatat jumlah alat berdasarkan jenis
 alat_kesehatan = {}
 alat_yang_dipinjam = set()
 
-# Fungsi untuk menambahkan alat ke dalam dictionary
+# Fungsi tambah alat dengan dua parameter jenis, jumlah dengan type data string
 def tambah_alat(nama_alat, jumlah):
     if nama_alat in alat_kesehatan:
         alat_kesehatan[nama_alat] += jumlah
@@ -89,7 +12,7 @@ def tambah_alat(nama_alat, jumlah):
     if jumlah > 0:
         alat_yang_dipinjam.add(nama_alat)
 
-# Fungsi untuk memproses pengembalian alat
+# Fungsi kurangi alat dengan dua parameter jenis, jumlah dengan type data string
 def kembalikan_alat(nama_alat, jumlah):
     if nama_alat in alat_kesehatan:
         alat_kesehatan[nama_alat] -= jumlah
@@ -114,16 +37,19 @@ while True:
 
     pilihan = input("Pilih opsi: ")
 
+    # jika variabel pilihan sama dengan 1 jalankan program di bawah ini
     if pilihan == "1":
         nama_alat = input("Masukkan nama alat: ")
         jumlah = int(input("Masukkan jumlah alat yang dipinjam: "))
         tambah_alat(nama_alat, jumlah)
 
+    # jika variabel pilihan tidak dengan 1 apakah variabel pilihan sama dengan 2 jalankan program di bawah ini
     elif pilihan == "2":
         nama_alat = input("Masukkan nama alat: ")
         jumlah = int(input("Masukkan jumlah alat yang dikembalikan: "))
         kembalikan_alat(nama_alat, jumlah)
 
+    # jika variabel pilihan tidak dengan 2 apakah variabel pilihan sama dengan 3 jalan kan program di bawah ini
     elif pilihan == "3":
         nama_alat_kembali = input("Masukkan nama alat yang dikembalikan: ")
         jumlah_kembali = int(input("Masukkan jumlah alat yang dikembalikan: "))
@@ -131,6 +57,7 @@ while True:
         jumlah_ambil = int(input("Masukkan jumlah alat yang diambil: "))
         tukar_alat(nama_alat_ambil, jumlah_ambil, nama_alat_kembali, jumlah_kembali)
 
+    # jika variabel pilihan tidak dengan 3 apakah variabel pilihan sama dengan 4 jalan kan program di bawah ini
     elif pilihan == "4":
         print("\nAlat yang masih dipinjam saat ini:")
         alat_dipinjam_sekarang = {alat: jumlah for alat, jumlah in alat_kesehatan.items() if jumlah > 0}
